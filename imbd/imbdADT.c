dc039086a2d225ba010821ff5326eae0e2e3128a
brunibaumgart <bbaumgart@itba.edu.ar>
2021-12-06 21:02:46 -0300

    Commit0

---
+++ imbd/imbdADT.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define TOP 5
+
+typedef struct film{
+    char * name;
+    long votes;
+    double rating;
+    char ** genres;
+}TFilm;
+
+typedef struct genres{
+    char * genre;
+    size_t qtyFilms;
+    size_t qtySeries;
+    struct genres * next;
+}TGenres;
+
+typedef TGenres * TListGenre;
+
+typedef struct year{
+    size_t year;
+    TListGenre firstGenre;
+    size_t qtyFilms;
+    size_t qtySeries;
+    size_t qtyShorts;
+    TFilm topFilms[TOP];
+    struct year * next;
+}TYear;
+
+typedef TYear * TListYear;
+
+typedef struct imbdCDT{
+    TListYear first;
+    size_t qtyYears;
+}imbdCDT;
