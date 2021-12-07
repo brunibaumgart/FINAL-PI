608845327ca1ab8f0fba7783d60709f538a4859c
brunibaumgart <bbaumgart@itba.edu.ar>
2021-12-07 17:44:21 -0300

    Correccion nombre carpeta

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <errno.h>
-#include "imbd\imdbADT.h"
+#include "imdb\imdbADT.h"
 
 #define ARGMTS 3       //Cantidad de argumentos a leer
 #define QUERIES 3      //Cantidad de queries
@@ -103,7 +103,7 @@ int main(int argc, char *argv[])
             }
         }
         if(!invalidYear){
-            
+
         }
     }
 }
